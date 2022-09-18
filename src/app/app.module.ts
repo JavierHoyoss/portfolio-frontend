1224599fc6d34754b0baca7223eaeadf7a9d292a
javier <sebastianhoyos12@gmail.com>
2022-09-18 09:56:18 -0300

    nav bar y acerca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaComponent } from './componentes/acerca/acerca.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaComponent,
+    NavbarComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
